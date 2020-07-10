0448bb4c4b1a37979fccbd46804cea7ea604f864
Salaudeen Ismail <ismailsalaudeen6696@gmail.com>
2020-07-11 00:42:55 +0100

    Added Some Comments

--- app.js
+++ app.js
@@ -3,18 +3,22 @@ let stopBtn = document.getElementById("stop");
 let hourInput = document.querySelector("input[name=hour]");
 let minInput = document.querySelector("input[name=minute]");
 let secInput = document.querySelector("input[name=seconds]");
+let info = document.getElementById("info-text");
 let timeoutID;
 
 function test() {
     if (secInput.value === '' && minInput.value === '' && hourInput.value === '' || hourInput.value == 0 && secInput.value == 0 && minInput.value == 0) {
+        info.innerHTML = "Fields are empty.";
         console.log("Fields are empty.");
     } else if (hourInput.value > 99 || minInput.value > 59 || secInput.value > 59) {
-        console.log("Incorrect inputs")
+        info.innerHTML = "Incorrect input(s).";
+        console.log("Incorrect inputs.")
     } else {
         // console.log(hourInput.value);
         // console.log(minInput.value);
         // console.log(secInput.value);
         // playBtn.innerText = "PAUSE";
+        info.innerHTML = "TIME WAITS FOR NO MAN!"
         playBtn.removeEventListener("click", test)
         timeoutID = window.setInterval(timeCounter, 1*1000);        
     }
@@ -25,7 +29,8 @@ function timeCounter() {
         secInput.value -= 1;
         if (secInput.value == 0) {
             window.clearInterval(timeoutID);
-            playBtn.addEventListener("click", test)
+            playBtn.addEventListener("click", test);
+            info.innerHTML = "TIME'S UP!!!"
             console.log("Done!")
         }
     } else if (minInput.value > 0) {
@@ -52,6 +57,9 @@ function stop() {
     minInput.value = 0;
     secInput.value = 0;
     window.clearInterval(timeoutID);
+    playBtn.addEventListener("click", test)
+
+    info.innerHTML = "Oh Oh. Time Stopped For You?! That's Amazing!"
     console.log("Stopped!");
 
 }
